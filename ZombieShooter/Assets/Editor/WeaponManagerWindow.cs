4d5ebf5792374686532b28e86f3437a3bd74719d
st3veV <stepan.vyterna@gmail.com>
2015-09-24 07:52:39 +0200

    starting with weapon database + editor window

---
+++ ZombieShooter/Assets/Editor/WeaponManagerWindow.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class WeaponManagerWindow : EditorWindow {
+
+    public WeaponDatabase Database { get; set; }
+
+    [MenuItem("ZombieShooter/Weapons")]
+    public static void Init()
+    {
+        WeaponManagerWindow window = EditorWindow.GetWindow<WeaponManagerWindow>();
+        window.Database = WeaponDatabase.Instance;
+        window.Show();
+    }
+
+    private int selectedWeaponIndex = -1;
+
+    void OnGUI()
+    {
+        Rect weaponSelectionAreaSize = new Rect(0, 0, position.width*.3f, position.height - 100f);
+        Rect weaponDetailsAreaSize = new Rect(0, 0, position.width*.7f, position.height - 100f);
+
+        GUILayout.Label("Weapon manager",EditorStyles.boldLabel);
+        GUILayout.BeginHorizontal();
+
+        //list
+        GUILayout.BeginVertical();
+        GUILayout.BeginHorizontal();
+        bool addWeaponPressed = GUILayout.Button("Add");
+        if(selectedWeaponIndex > -1)
+        {
+            bool removeWeaponPressed = GUILayout.Button("Remove", GUIStyle.none);
+        }
+        GUILayout.EndHorizontal();
+        GUILayout.BeginArea(weaponSelectionAreaSize);
+        GUILayout.BeginVertical();
+        selectedWeaponIndex = -1;
+        int i = 0;
+        /*
+        foreach (IWeapon weapon in Database.weapons)
+        {
+            bool selected = EditorGUILayout.Foldout(false, weapon.Name);
+            if(selected)
+            {
+                selectedWeaponIndex = i;
+            }
+            i++;
+        }
+        */
+        GUILayout.EndVertical();
+        GUILayout.EndArea();
+        GUILayout.EndVertical();
+
+        GUILayout.BeginArea(weaponDetailsAreaSize);
+        GUILayout.BeginVertical();
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Name");
+        string weaponName = GUILayout.TextField("New weapon");
+        GUILayout.EndHorizontal();
+
+        GUILayout.Button("Save");
+        GUILayout.EndVertical();
+        GUILayout.EndArea();
+
+        GUILayout.EndHorizontal();
+    }
+
+}
---
+++ ZombieShooter/Assets/Scripts/WeaponSystem/WeaponDatabase.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class WeaponDatabase : ScriptableObject {
+
+    private static string databaseName = "WeaponDB";
+    private static WeaponDatabase _instance;
+    public static WeaponDatabase Instance
+    {
+        get
+        {
+            _instance = Resources.Load(databaseName, typeof(WeaponDatabase)) as WeaponDatabase;
+            if(_instance == null)
+            {
+                _instance = ScriptableObject.CreateInstance<WeaponDatabase>();
+#if UNITY_EDITOR
+                AssetDatabase.CreateAsset(_instance, "Assets/Resources/"+databaseName+".asset");
+#endif
+            }
+            if (_instance.weapons == null)
+            {
+                _instance.weapons = new List<IWeapon>();
+            }
+            return _instance;
+        }
+    }
+
+    public List<IWeapon> weapons;
+
+}
