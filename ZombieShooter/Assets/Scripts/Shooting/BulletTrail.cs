0e2ad7ff31f7e642c5dc0167d0f996c06dabfecd
st3veV <stepan.vyterna@gmail.com>
2015-12-03 07:59:29 +0100

    fixing race condition in bullet trail

--- ZombieShooter/Assets/Scripts/Shooting/BulletTrail.cs
+++ ZombieShooter/Assets/Scripts/Shooting/BulletTrail.cs
@@ -16,9 +16,16 @@ namespace Shooting
         public Action<GameObject> OnDone;
         private Vector3 _origin;
 
+        private bool _scheduleUpdate = false;
+
         private void Start()
         {
             _lineRenderer = GetComponent<LineRenderer>();
+            if (_scheduleUpdate)
+            {
+                _scheduleUpdate = false;
+                EventManager.Instance.AddUpdateListener(AnimateUpdate);
+            }
         }
 
         public void SetTarget(Vector3 origin, Vector3 target)
@@ -27,32 +34,39 @@ namespace Shooting
             _destination = target;
             _distance = Vector3.Distance(origin, target);
             _currentStep = 0;
-            EventManager.Instance.AddUpdateListener(AnimateUpdate);
+            if (_lineRenderer)
+            {
+                EventManager.Instance.AddUpdateListener(AnimateUpdate);
+            }
+            else
+            {
+                _scheduleUpdate = true;
+            }
         }
 
         private void AnimateUpdate()
         {
-                if (_currentStep < _distance)
-                {
-                    float x0 = Mathf.Lerp(0, _distance, _currentStep);
-                    _currentStep += _distance/3;
-                    float x1 = Mathf.Lerp(0, _distance, _currentStep);
+            if (_currentStep < _distance)
+            {
+                float x0 = Mathf.Lerp(0, _distance, _currentStep);
+                _currentStep += _distance/3;
+                float x1 = Mathf.Lerp(0, _distance, _currentStep);
 
-                    Vector3 origin = _origin;
-                    Vector3 destination = _destination;
+                Vector3 origin = _origin;
+                Vector3 destination = _destination;
 
-                    Vector3 pos0 = x0*Vector3.Normalize(destination - origin) + origin;
-                    Vector3 pos1 = x1*Vector3.Normalize(destination - origin) + origin;
+                Vector3 pos0 = x0*Vector3.Normalize(destination - origin) + origin;
+                Vector3 pos1 = x1*Vector3.Normalize(destination - origin) + origin;
 
-                    _lineRenderer.SetPosition(0, pos0);
-                    _lineRenderer.SetPosition(1, pos1);
-                }
-                else
-                {
-                    EventManager.Instance.RemoveUpdateListener(AnimateUpdate);
-                    OnDone(gameObject);
-                }
-            
+                _lineRenderer.SetPosition(0, pos0);
+                _lineRenderer.SetPosition(1, pos1);
+            }
+            else
+            {
+                EventManager.Instance.RemoveUpdateListener(AnimateUpdate);
+                OnDone(gameObject);
+            }
+
         }
     }
 }
